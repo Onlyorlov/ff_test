d4e97c494a24a51e93b40d0703f0c8321d0f354f
Onlyorlov <31902329+Onlyorlov@users.noreply.github.com>
2020-08-27 19:41:53 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def SomeExperiment(val1):
 	print('Результаты измерений')
-	def func():
+	def func(val):
 		result = val1 + np.random.uniform(low=-1, high=1)
 		return np.round(result, 2)
 	text = ''
 	for i in range(10):
-		print('Результат ' + str(i) + ' измерения: ' + str(func()))
+		print('Результат ' + str(i) + ' измерения: ' + str(func(val1)))
 	return 
 
 
