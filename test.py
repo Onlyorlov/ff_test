75394ba9d4ba2e953a3b6914452b4f6646dc1a22
Onlyorlov <31902329+Onlyorlov@users.noreply.github.com>
2020-08-27 18:39:25 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+def SomeExperiment():
+	print('Результаты измерений')
+	def func():
+		return None
+	text = ''
+	for i in range(10):
+		print('Результат ' + str(i) + ' измерения: ' + str(func()))
+	return 
+
+
+def AnotherExperiment():
+	print('Хуйня какая-то')
